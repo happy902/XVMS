4b473f5f84834573e418770acf5827b7c653d6b5
Tonkercke <another2468@protonmail.com>
2022-04-16 13:16:57 +0900

    2022-0416

--- configure.sh
+++ configure.sh
@@ -18,7 +18,7 @@ cat << EOF > /usr/local/etc/xray/config.json
   },
   "inbounds": [
     {
-      "port": $PORT,
+      "port": 443,
       "protocol": "VMESS",
       "settings": {
         "clients": [
