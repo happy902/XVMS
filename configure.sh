5e94fca0ea1fcccddf211a65b70f9dba5be5e66f
Tonkercke <another2468@protonmail.com>
2022-05-10 14:15:02 +0900

    2022-0510

--- configure.sh
+++ configure.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-# V2Ray generate configuration
-# Download and install V2Ray
+# xray generate configuration
+# Download and install xray
 config_path=$PROTOCOL"_ws_tls.json"
 mkdir /tmp/xray
 curl -L -H "Cache-Control: no-cache" -o /tmp/xray/xray.zip https://github.com/XTLS/Xray-core/releases/latest/download/Xray-linux-64.zip
@@ -17,6 +17,6 @@ cd /opt/test
 dd if=/dev/zero of=100mb.bin bs=100M count=1
 dd if=/dev/zero of=10mb.bin bs=10M count=1
 # Run xray
-/usr/local/bin/v2ray -config /usr/local/etc/xray/config.json &
+/usr/local/bin/xray -config /usr/local/etc/xray/config.json
 # Run nginx
 /bin/bash -c "envsubst '\$PORT,\$WS_PATH' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf" && nginx -g 'daemon off;'
